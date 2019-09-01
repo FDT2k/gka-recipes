4e2693727de49523788fdd5052e7cbc359f8b2f2
Fabien Di Tore <fabien@ditore.ch>
2019-09-01 11:55:49 +0200

    added recipes && fixed bugs

--- src/recipes/package_json/babel-node-build-nopub/index.js
+++ src/recipes/package_json/babel-node-build-nopub/index.js
@@ -35,9 +35,11 @@ exports.v[0] =  (context)=>{
   package_json['scripts']['distclean'] = `rm -rf ./${dist_folder}`
   package_json['scripts']['build'] = `npm run test && node_modules/@babel/cli/bin/babel.js --out-dir ${dist_folder} ./src && cp package.json ./${dist_folder}`
   package_json['scripts']['build:clean'] = `npm run distclean; npm run build`
-  package_json['scripts']['build:patch'] =`npm run build && npm version patch`
-  package_json['scripts']['build:minor'] =`npm run build && npm version minor`
-  package_json['scripts']['build:major'] =`npm run build && npm version major`
+  package_json['scripts']['pub'] =  `cd ./${dist_folder}; npm publish; cd ..`
+  package_json['scripts']['build:pub'] = `npm run build && npm run pub`
+  package_json['scripts']['build:patch:pub'] =`npm run build && npm version patch && npm run pub`
+  package_json['scripts']['build:minor:pub'] =`npm run build && npm version minor && npm run pub`
+  package_json['scripts']['build:major:pub'] =`npm run build && npm version major && npm run pub`
 
   package_json['devDependencies'] = Object.assign({},package_json['devDependencies'],require(path.join(pathes.files,'devDeps.json')))
 
--- src/recipes/package_json/clean-scripts/index.js
+++ src/recipes/package_json/clean-scripts/index.js
@@ -10,9 +10,9 @@ exports.v[0] =  (context)=>{
 
 
 
-  log.log('setting up build with babel (es6) ')
+  log.log('Cleaning up scripts in package.json')
 
-  let {package_dir,project_dir,path:dest_path,node} = validate(varStore(),['package_dir','project_dir','path',['node']]);
+  let {package_dir,project_dir,path:dest_path} = validate(varStore(),['package_dir','project_dir','path']);
 
   let temp_folder = path.join('/tmp',uuid())
 
@@ -20,26 +20,26 @@ exports.v[0] =  (context)=>{
     files: path.join(__dirname,'files'),
     dest:path.join(package_dir,'package.json'),
   }
+/*
+  let preserve_list = [];
 
-  let dist_folder = 'lib';
-
-  console.log(node)
-  if( typeof node !== 'undefined' && typeof node.dist_folder_name != 'undefined')
-    dist_folder= node.dist_folder_name;
-
+  console.log(npm)
+  if( typeof npm !== 'undefined' && typeof npm.preserve_scripts != 'undefined')
+    preserve_list= node.preserve_scripts;
+*/
   let package_json = require (pathes.dest)
-  if(typeof package_json['scripts'] == 'undefined')
-    package_json['scripts']={};
+  //if(typeof package_json['scripts'] == 'undefined')
+  package_json['scripts']={};
 
-  package_json['scripts']['start'] = `npx nodemon node_modules/@babel/node/bin/babel-node.js src/index.js`
+  /*package_json['scripts']['start'] = `npx nodemon node_modules/@babel/node/bin/babel-node.js src/index.js`
   package_json['scripts']['distclean'] = `rm -rf ./${dist_folder}`
   package_json['scripts']['build'] = `npm run test && node_modules/@babel/cli/bin/babel.js --out-dir ${dist_folder} ./src && cp package.json ./${dist_folder}`
   package_json['scripts']['build:clean'] = `npm run distclean; npm run build`
   package_json['scripts']['build:patch'] =`npm run build && npm version patch`
   package_json['scripts']['build:minor'] =`npm run build && npm version minor`
-  package_json['scripts']['build:major'] =`npm run build && npm version major`
+  package_json['scripts']['build:major'] =`npm run build && npm version major`*/
 
-  package_json['devDependencies'] = Object.assign({},package_json['devDependencies'],require(path.join(pathes.files,'devDeps.json')))
+//  package_json['devDependencies'] = Object.assign({},package_json['devDependencies'],require(path.join(pathes.files,'devDeps.json')))
 
   let newPackage = JSON.stringify(package_json,null,'\t');
 
@@ -48,13 +48,6 @@ exports.v[0] =  (context)=>{
       path:pathes.dest,
       data:newPackage,
       describe:()=>`write ${newPackage.length} bytes to ${pathes.dest}`
-    }),
-    cli.tasks.copy({
-      source:path.join(__dirname,'files','.babelrc'),
-      dest: package_dir,
-      cwd:project_dir,
-      cond:()=>!fs.existsSync(path.join(package_dir,'.babelrc')),
-      describe:()=>`copying babelrc into ${package_dir}`,
     })
 
   ])
